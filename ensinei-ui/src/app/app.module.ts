318dd045dda591130e0421e2c6bd06c1c1fa0304
lucasgoes <lucasdegoes@outlook.com.br>
2019-10-02 18:46:10 -0300

    iniciando com angular

---
+++ ensinei-ui/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule}  from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PainelNegociacaoComponent } from './painel-negociacao/painel-negociacao.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PainelNegociacaoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
